77a5aba5a84a7301e6d3f981eaddee65477fe1f5
HP <HP@DESKTOP-8EO1B9C>
2020-12-16 18:57:51 +0300

    feat: add component RadioInput

---
+++ src/components/RadioInput/RadioInput.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import styled from 'styled-components';
+
+interface IProps {
+  id: string
+  value: string
+  currentSearch: string
+  onChange: () => void
+}
+
+export function RadioInput({
+  id, value, currentSearch, onChange,
+}: IProps) {
+  // const [isChecked, setIsChecked] = useState(false);
+
+  // const onChangeHandler = useCallback((event) => {
+  //   if (event.target.checked) {
+  //     onChange(id);
+  //     setIsChecked(event.target.checked);
+  //   }
+  // }, [id, onChange]);
+
+  return (
+    <>
+      <StyledInput type="radio" id={id} onChange={onChange} checked={currentSearch === id} />
+      <StyledLabel htmlFor={id} isChecked={currentSearch === id}>{value}</StyledLabel>
+    </>
+  );
+}
+
+const StyledInput = styled.input`
+  visibility: hidden;
+  display: none;
+`;
+
+const StyledLabel = styled.label<{ isChecked: boolean }>`
+  margin-right: 5px;
+  padding: 5px 20px;
+  font-size: 14px;
+  color: #FFFFFF;
+  text-transform: uppercase;
+  cursor: pointer;
+
+  ${(props) => (props.isChecked
+    ? 'background: #F65263'
+    : 'background: #3B3C3C'
+  )}
+`;
