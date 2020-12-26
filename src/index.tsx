4dbbcc4e8ae8f5536b015bb0d57c3fbf4bd0f541
HP <HP@DESKTOP-8EO1B9C>
2020-12-26 19:59:54 +0300

    added HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './index.css';
 import { Provider } from 'react-redux';
 import { App } from './App';
@@ -8,11 +8,11 @@ import { store } from './store';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter basename="https://github.com/AnnLuschik/Cinema-App/">
       <Provider store={store}>
         <App />
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root'),
 );
