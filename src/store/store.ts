797990d1c387458757e738edb32cba32f8dd540d
HP <HP@DESKTOP-8EO1B9C>
2020-12-27 11:53:14 +0300

    refactor: removed commented code

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,6 +1,4 @@
 import { configureStore } from '@reduxjs/toolkit';
-// import { applyMiddleware } from 'redux';
-// import { composeWithDevTools } from 'redux-devtools-extension';
 import { rootReducer } from './rootResucer';
 import {
   movieSearchMiddleware,
@@ -12,19 +10,3 @@ export const store = configureStore({
   reducer: rootReducer,
   middleware: [movieSearchMiddleware, moreMovieSearchMiddleware, singleSearchMiddleware],
 });
-
-// export const store: Store = createStore(rootReducer, applyMiddleware(movieSearchMiddleware));
-
-// export const store = configureStore({
-//   reducer: {
-//     movie: movieSearchReducer,
-//   },
-// });
-
-// export type RootState = ReturnType<typeof store.getState>;
-// export type AppThunk<ReturnType = void> = ThunkAction<
-// ReturnType,
-// RootState,
-// unknown,
-// Action<string>
-// >;
