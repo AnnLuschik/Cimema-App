19c128339e1fddc4ffd5a5758ea66067d7ce0e5a
HP <HP@DESKTOP-8EO1B9C>
2021-01-09 00:37:16 +0300

    feat: add responsive styles for 404 page

--- src/Movie/components/NotFoundPage/NotFoundPage.tsx
+++ src/Movie/components/NotFoundPage/NotFoundPage.tsx
@@ -17,47 +17,59 @@ export function NotFoundPage() {
 }
 
 const Container = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    width: 100vw;
-    height: 90vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100vw;
+  height: 90vh;
+
+  @media (max-width: 600px) {
+    flex-direction: column;
+  }
 `;
 
 const TextContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: top;
-    align-items: center;
-    width: 400px;
-    height: 400px;
+  display: flex;
+  flex-direction: column;
+  justify-content: top;
+  align-items: center;
+  margin-bottom: 15px;
 `;
 
 const StyledTitle = styled.p`
-    margin-bottom: 25px;
-    font-weight: 900;
-    font-size: 90px;
-    color: #3F806D;
-    text-transform: uppercase;
+  margin-bottom: 25px;
+  font-weight: 900;
+  font-size: calc(60px + 2.2vw);
+  color: #3F806D;
+  text-transform: uppercase;
 `;
 
 const StyledText = styled.p`
-    margin-bottom: 50px;
-    font-weight: 400;
-    font-size: 30px;
-    color: #3F806D;
-    text-transform: none;
+  margin-bottom: 50px;
+  font-weight: 400;
+  font-size: calc(20px + 0.7vw);
+  color: #3F806D;
+  text-transform: none;
 `;
 
 const StyledImg = styled.img`
-    width: 500px;
-    height: 400px;
+  width: 37vw;
+  height: 30vw;
+
+  @media (max-width: 600px) {
+    width: 75vw;
+    height: 70vw;
+  }
 `;
 
 const StyledLink = styled(Link)`
-    justify-self: bottom;
-    padding: 10px 15px;
-    color: #FFFFFF;
-    background: #3F806D;
-    text-decoration: none;
+  justify-self: bottom;
+  padding: 10px 15px;
+  color: #FFFFFF;
+  background: #3F806D;
+  text-decoration: none;
+
+  &:hover {
+    background: #5aa78f;
+  } 
 `;
